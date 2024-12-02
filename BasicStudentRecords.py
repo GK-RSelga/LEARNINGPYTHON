7c0fcb2edad3c9e4e65d8767d08a0a890540c6fe
Rowell Selga <masterwi.mw@gmail.com>
2024-12-02 16:34:42 +0800

    Update BasicStudentRecords.py

--- BasicStudentRecords.py
+++ BasicStudentRecords.py
@@ -1,1 +1,10 @@
-
+# Initialize an empty list to store student names
+student_list = []
+# Import colorama for colored text output
+import colorama
+colorama.init()
+# Print using colorama for the header of student names
+print(colorama.Fore.GREEN + "\n=== Enter Student Names === " + colorama.Style.RESET_ALL)
+# Using For loops to input the names of the student
+for x in range(1, 4):
+    # Prompt
